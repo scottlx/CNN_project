eb1ed64c8fc34b799cc652636965e78656ff8d1c
Xun Lin <xunlin@bu.edu>
2018-10-19 22:42:11 -0400

    fixed shuffle bug

--- keras_classifier.py
+++ keras_classifier.py
@@ -5,7 +5,9 @@ from tensorflow.keras.layers import Convolution2D
 from tensorflow.keras.layers import MaxPooling2D
 from tensorflow.keras.layers import Flatten
 from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Dropout
 from tensorflow.keras.models import load_model
+from keras import backend as K
 import numpy as np
 import pandas as pd
 from google.colab import files
@@ -13,15 +15,37 @@ import os
 import tensorflow as tf
 
 
+# dimensions of our images.
+img_width, img_height = 150, 150
+
+train_data_dir = '/content/drive/My Drive/flowers/train'
+validation_data_dir = '/content/drive/My Drive/flowers/validation'
+nb_train_samples = 1616
+nb_validation_samples = 200
+epochs = 50
+batch_size = 16
+
+if K.image_data_format() == 'channels_first':
+    input_shape = (3, img_width, img_height)
+else:
+    input_shape = (img_width, img_height, 3)
+
+TPU_WORKER = 'grpc://' + os.environ['COLAB_TPU_ADDR']
+
 classifier = Sequential()
-classifier.add(Convolution2D(32, (3, 3), input_shape = (64, 64, 3), activation = 'relu'))
-classifier.add(MaxPooling2D(pool_size = (2, 2), strides = (2, 2)))
-classifier.add(Convolution2D(64, (5, 5), activation = 'relu'))
+classifier.add(Convolution2D(32, (3, 3),input_shape = input_shape, activation = 'relu'))
 classifier.add(MaxPooling2D(pool_size = (2, 2)))
+classifier.add(Convolution2D(32, (3, 3), activation = 'relu'))
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+classifier.add(Convolution2D(64, (3, 3),activation = 'relu'))
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+
+
 classifier.add(Flatten())
-classifier.add(Dense(activation="relu", units=512))
+classifier.add(Dense(activation="relu", units=64))
+classifier.add(Dropout(0.5))
 classifier.add(Dense(activation="sigmoid", units=1))
-classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+classifier.compile(optimizer = 'rmsprop', loss = 'binary_crossentropy', metrics = ['accuracy'])
 
 tpu_model = tf.contrib.tpu.keras_to_tpu_model(
 classifier,
@@ -31,43 +55,50 @@ strategy=tf.contrib.tpu.TPUDistributionStrategy(
 from keras.preprocessing.image import ImageDataGenerator
 
 train_datagen = ImageDataGenerator(
-    rotation_range=0.2,
-    width_shift_range=0.2,
-    height_shift_range=0.2,
     rescale=1./255,
     shear_range=0.2,
     zoom_range=0.2,
     horizontal_flip=True,
-    fill_mode='nearest'
 )
 
 test_datagen = ImageDataGenerator(
     rescale=1./255
 )
 
-training_set = train_datagen.flow_from_directory(
-    '/content/drive/My Drive/flowers_training',
-    target_size=(64, 64),
-    batch_size=32,
+train_generator = train_datagen.flow_from_directory(
+    train_data_dir,
+    target_size=(img_width, img_height),
+    batch_size=batch_size,
     class_mode='binary'
 )
 
-test_set= test_datagen.flow_from_directory(
-    '/content/drive/My Drive/flowers_test',
-    target_size=(64, 64),
-    batch_size=32,
+validation_generator= test_datagen.flow_from_directory(
+    validation_data_dir,
+    target_size=(img_width, img_height),
+    batch_size=batch_size,
     class_mode='binary'
 )
 
 classifier.fit_generator(
-    training_set,
-    steps_per_epoch=400,
-    epochs=10,
+    train_generator,
+    steps_per_epoch=nb_train_samples // batch_size,
+    epochs=epochs,
+    validation_data=validation_generator,
+    validation_steps=nb_validation_samples // batch_size
 )
 
-
+test_datagen = ImageDataGenerator(
+    rescale=1./255,
+)
+test_set= test_datagen.flow_from_directory(
+    '/content/drive/My Drive/flowers_test',
+    target_size=(img_width, img_height),
+    batch_size=16,
+    class_mode='binary',
+    shuffle =False
+)
 test_set.reset()
-pred=tpu_model.predict_generator(test_set,verbose=1, steps=1)
+pred=classifier.predict_generator(test_set,verbose=1, steps = 1)
 print(pred)
 
 predictions = [round(x[0]) for x in pred]
